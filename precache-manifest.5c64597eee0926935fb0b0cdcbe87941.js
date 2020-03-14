98e3d9e6b78c3f630c2700482a5dd64b785b2de3
Gurpreet <gurpreet1161@gmail.com>
2020-03-14 22:18:45 +1200

    Updates

---
+++ precache-manifest.5c64597eee0926935fb0b0cdcbe87941.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da9fe675bf15d15585ce847818f8d3e7",
+    "url": "/deploymenttest/index.html"
+  },
+  {
+    "revision": "181eb7175c27c87eba81",
+    "url": "/deploymenttest/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "863cdffffd8382881567",
+    "url": "/deploymenttest/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "181eb7175c27c87eba81",
+    "url": "/deploymenttest/static/js/2.170660c4.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/deploymenttest/static/js/2.170660c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "863cdffffd8382881567",
+    "url": "/deploymenttest/static/js/main.30463170.chunk.js"
+  },
+  {
+    "revision": "30428959028a93449043",
+    "url": "/deploymenttest/static/js/runtime-main.c3cfa0a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploymenttest/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/deploymenttest/precache-manifest.cadcd52e6f99a87cda5f484532193e45.js"
+  "/deploymenttest/precache-manifest.5c64597eee0926935fb0b0cdcbe87941.js"
 );
 
 self.addEventListener('message', (event) => {
